83fe044afeb6272817fabb0b3bc35d7f993b32f8
root <ifedayoadeniyi@gmail.com>
2023-10-12 15:05:56 +0000

    feat: update max year to 2022

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -2,7 +2,7 @@
 -- Rank `glam rock` bands by longevity.
 --
 
-SELECT band_name, (IFNULL(split, 2020) - formed) AS lifespan
+SELECT band_name, (IFNULL(split, 2022) - formed) AS lifespan
 FROM metal_bands
 WHERE IFNULL(FIND_IN_SET('Glam rock', style), 0) > 0
 ORDER BY lifespan DESC;
