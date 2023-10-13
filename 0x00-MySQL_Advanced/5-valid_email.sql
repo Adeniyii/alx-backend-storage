a0bcac947dd5e886585319a8f53422ab47196f8f
root <ifedayoadeniyi@gmail.com>
2023-10-13 00:19:15 +0000

    feat: improve trigger validation

--- 0x00-MySQL_Advanced/5-valid_email.sql
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -2,11 +2,19 @@
 -- Email validation trigger.
 --
 
+DROP TRIGGER IF EXISTS validate_email_before;
+
+DELIMITER $$
+
 CREATE TRIGGER validate_email_before
 BEFORE UPDATE
 ON users FOR EACH ROW
-IF NEW.email NOT REGEXP '^[^@]+@[^@]+\.[^@]{2,}$' THEN
-	UPDATE users SET valid_email = 0 WHERE id = NEW.id;
-ELSE 
-	UPDATE users SET valid_email = 1 WHERE id = NEW.id;
-END IF;
+BEGIN
+	IF NEW.email != OLD.email THEN
+		SET NEW.valid_email = 0;
+	ELSE 
+		SET NEW.valid_email = 1;
+	END IF;
+END $$
+
+DELIMITER ;
