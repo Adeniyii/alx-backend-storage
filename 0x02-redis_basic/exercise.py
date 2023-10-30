22aa806c55d52b999ed6a36e5ea894bd9cf35c42
Adeniyii <ifedayoadeniyi@gmail.com>
2023-10-30 12:55:33 +0100

    add comments

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,19 +1,19 @@
 #!/usr/bin/env python3
-""""""
+"""exercise.py module defines a cache class and inits redis."""
 from typing import Union
 import redis
 import uuid
 
 
 class Cache():
-    """"""
+    """Cache class."""
 
     def __init__(self) -> None:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
     def store(self, data: Union[int, str, bytes, float]):
-        """"""
+        """Store value into redis database."""
         key: str = str(uuid.uuid4())
         self._redis.set(key, data)
 
