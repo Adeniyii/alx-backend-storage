106f5eba793ba8f9c59a706798eca10b24cf63bf
Adeniyii <ifedayoadeniyi@gmail.com>
2023-10-30 12:54:10 +0100

    merge?

--- 0x01-NoSQL/11-schools_by_topic.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-"""11-schools_by_topic defines a function `schools_by_topic` which fetches all records
-which have the given topic."""
-
-
-def schools_by_topic(mongo_collection, topic):
-    """fetch all records which contains the given topic."""
-    res = mongo_collection.find({'topics': {'$all': [topic]}})
-    return res
