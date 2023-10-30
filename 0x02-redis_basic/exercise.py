3eac809b9b1a0af2f9e5c6dc9e7fc19f10a8981e
Adeniyii <ifedayoadeniyi@gmail.com>
2023-10-30 13:19:29 +0100

    feat: implement get method for redis cache

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """exercise.py module defines a cache class and inits redis."""
-from typing import Union
+from typing import Any, Callable, Union
 import redis
 import uuid
 
@@ -18,3 +18,24 @@ class Cache():
         self._redis.set(key, data)
 
         return key
+
+    def get(self, key: str, fn: Callable[[Union[bytes, None]], Any]):
+        """Get and convert value from redis db."""
+        v = self._redis.get(key)
+        if fn is not None:
+            return fn(v)
+        return v
+
+
+if __name__ == "__main__":
+    cache = Cache()
+
+    TEST_CASES = {
+        b"foo": None,
+        123: int,
+        "bar": lambda d: d.decode("utf-8")
+    }
+
+    for value, fn in TEST_CASES.items():
+        key = cache.store(value)
+        assert cache.get(key, fn=fn) == value
