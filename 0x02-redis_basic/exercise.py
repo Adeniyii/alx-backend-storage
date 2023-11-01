641a2a6b76d87b3ff75d930fda05fadcfac5343f
Adeniyii <ifedayoadeniyi@gmail.com>
2023-11-01 13:06:23 +0100

    test impl

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -81,9 +81,17 @@ class Cache():
 if __name__ == "__main__":
     cache = Cache()
 
-    cache.store(b"first")
-    print(cache.get(cache.store.__qualname__))
+    s1 = cache.store("first")
+    print(s1)
+    s2 = cache.store("second")
+    print(s2)
+    s3 = cache.store("third")
+    print(s3)
 
-    cache.store(b"second")
-    cache.store(b"third")
-    print(cache.get(cache.store.__qualname__))
+    inputs = cache._redis.lrange(
+        "{}:inputs".format(cache.store.__qualname__), 0, -1)
+    outputs = cache._redis.lrange(
+        "{}:outputs".format(cache.store.__qualname__), 0, -1)
+
+    print("inputs: {}".format(inputs))
+    print("outputs: {}".format(outputs))
